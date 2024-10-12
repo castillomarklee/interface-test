c54ea170ac886f8b7eb50ac872512a6b9d51eebb
Mark Lee Castillo <mark.c@portalink.com>
2024-10-12 19:24:50 +0800

    add scroll event detection for component 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+'use client'
+
 import { Box, ScaleFade } from '@chakra-ui/react'
-import styles from './page.module.scss'
 import ComponentOne from './components/component-one'
 import ComponentTwo from './components/component-two'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
+  const compTwoRef = useRef<HTMLDivElement | null>(null)
+
+  const [isShowSecondComp, setIsShowSecondComp] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      const entry = entries[0]
+      setIsShowSecondComp(entry.isIntersecting)
+    })
+
+    observer.observe(compTwoRef.current as Element)
+  }, [])
+
   return (
     <Box
       w="100"
@@ -15,7 +30,7 @@ export default function Home() {
       <ScaleFade initialScale={0.9} in={true}>
         <ComponentOne />
       </ScaleFade>
-      <ScaleFade initialScale={0.9} in={true}>
+      <ScaleFade ref={compTwoRef} initialScale={0.9} in={isShowSecondComp}>
         <Box mt="200px">
           <ComponentTwo />
         </Box>
